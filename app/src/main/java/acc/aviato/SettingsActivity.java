609a55076f9ebad19919055fd77a828d1eaacec8
Michael <Michael>
2015-11-18 11:25:02 -0600

    Create logout button and add logout functionality

--- app/src/main/java/acc/aviato/SettingsActivity.java
+++ app/src/main/java/acc/aviato/SettingsActivity.java
@@ -1,13 +1,33 @@
 package acc.aviato;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.ParseUser;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    Button mLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mLogOut = (Button)findViewById(R.id.logOutButton);
+
+        mLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser.logOut();
+                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
 }
