b1d01c4b259796b4ffe22f66785e6d60b6fe2d93
Michael <Michael>
2015-11-18 23:29:48 -0600

    finishing touches on rebase

--- app/src/main/java/acc/aviato/SettingsActivity.java
+++ app/src/main/java/acc/aviato/SettingsActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -21,14 +23,19 @@ public class SettingsActivity extends AppCompatActivity {
         mLogOut = (Button)findViewById(R.id.logOutButton);
         user = ParseUser.getCurrentUser();
         if(user==null) {
-            mLogOut.setText("Sign in");
+            mLogOut.setText(R.string.login);
         }
 
         mLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(user==null) {
-                    ParseUser.logOut();
+                if(user!=null) {
+                    ParseUser.logOutInBackground(new LogOutCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            //Logout finished
+                        }
+                    });
                 }
                 Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
